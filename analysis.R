274854c263de6da2f6dde8c380d30dfdd6f447c0
xy@工作間 <innerfirexy@gmail.com>
2021-08-11 11:07:12 -0700

    collect

--- analysis.R
+++ analysis.R
@@ -16,3 +16,20 @@ d1[,.(AvgSDMeanRatio = mean(SDMeanRatio)), by = .(size, speed)]
 # 7:  200 0.720      0.4194786
 # 8:  200 0.960      0.4056542
 # 9:  200 1.200      0.4954646
+
+
+d2 = fread("results_without.csv")
+setnames(d2, c("path", "mean", "sd", "size", "speed"))
+
+d2$SDMeanRatio = d2$sd / d2$mean
+d2[,.(AvgSDMeanRatio = mean(SDMeanRatio)), by = .(size, speed)]
+#    size speed AvgSDMeanRatio
+# 1:   50 0.060      0.4389157
+# 2:   50 0.045      0.4888785
+# 3:   50 0.075      0.3681829
+# 4:  100 0.180      0.5295387
+# 5:  100 0.240      0.4633469
+# 6:  100 0.300      0.5064373
+# 7:  200 0.720      0.4496567
+# 8:  200 0.960      0.6785399
+# 9:  200 1.200      0.6473749
